deef394116787f8adb48cbb0dfb370100c557a85
Nduwayo Fabrice <nduwayof@outlook.com>
2019-09-23 12:29:57 -0500

    created the gitignore file

--- src/main/java/edu/mum/cs/dao/user/UserDao.java
+++ src/main/java/edu/mum/cs/dao/user/UserDao.java
@@ -2,19 +2,10 @@ package edu.mum.cs.dao.user;
 
 import edu.mum.cs.dao.GenericDao;
 import edu.mum.cs.domain.User;
-import org.hibernate.Session;
 
 public class UserDao extends GenericDao implements IUserDao {
 
     public UserDao() {
         typeParameterClass = User.class;
-    }
-
-    @Override
-    public User findById(long id) {
-        Session session = sessionFactory.openSession();
-        User user = session.get(User.class, id);
-        session.close();
-        return user;
     }
 }
