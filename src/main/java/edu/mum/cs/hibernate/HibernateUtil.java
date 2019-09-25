2bd7acb7f765e22fd0dcfa034170d6e6f9df2f35
Nduwayo Fabrice <nduwayof@outlook.com>
2019-09-25 12:12:56 -0500

    minor changes

--- src/main/java/edu/mum/cs/hibernate/HibernateUtil.java
+++ src/main/java/edu/mum/cs/hibernate/HibernateUtil.java
@@ -22,9 +22,9 @@ public class HibernateUtil {
                 Configuration configuration = new Configuration();
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/clickme-db?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
-                settings.put(Environment.USER, "clickme-db-sys");
-                settings.put(Environment.PASS, "1234");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/wapdb?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "hexakomb");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
